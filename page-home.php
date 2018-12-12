5aea5f31cdbc96bccd865fd98e85ea24f92f84d1
yoemrattana <yoemrattana168@gmail.com>
2018-12-12 23:54:50 +0700

    -publication image width

--- page-home.php
+++ page-home.php
@@ -248,7 +248,7 @@
 								<div class="post-block-style post-float clearfix">
 									<div class="post-thumb">
 										<a href="<?php the_permalink(); ?>">
-											<img class="img-fluid" src="<?php echo get_field( 'image' )['url'] ?>" alt="" />
+											<img class="img-fluid" src="<?php echo get_field( 'image' )['url'] ?>" alt=""  style="width:70px"/>
 										</a>
 										<a class="post-cat" href="#"><?php echo pll__( 'Publication' ) ?></a>
 									</div><!-- Post thumb end -->
