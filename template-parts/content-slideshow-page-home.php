835c712ad8c4b888379e04d94b67cafb546aa279
yoemrattana <yoemrattana168@gmail.com>
2018-12-22 14:27:22 +0700

    slide id changed

--- template-parts/content-slideshow-page-home.php
+++ template-parts/content-slideshow-page-home.php
@@ -2,8 +2,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12">
-				<?php echo do_shortcode('[metaslider id="254"]'); ?>
-				<?php //echo do_shortcode('[metaslider id="410"]'); ?>
+				<?php //echo do_shortcode('[metaslider id="254"]'); ?>
+				<?php echo do_shortcode('[metaslider id="410"]'); ?>
 			</div>
 		</div>
 	</div>
