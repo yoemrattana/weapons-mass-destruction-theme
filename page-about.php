4c8ff597ae72ae4ac575a3d9628690afb65da315
yoemrattana <yoemrattana168@gmail.com>
2018-12-23 14:52:56 +0700

    change font size and line size page content

--- page-about.php
+++ page-about.php
@@ -15,9 +15,13 @@
 					</h3>
 					<div class="row">
 						<div class="col-sm-12">
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-								<?php the_content(); ?>
-							<?php endwhile; endif; ?>
+							<div class="content-page">
+								<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+								
+									<?php the_content();?>
+									
+								<?php endwhile; endif; ?>
+							</div>
 						</div>
 			      	</div><!-- Post block row end -->
 				</div><!--More news block end -->				
