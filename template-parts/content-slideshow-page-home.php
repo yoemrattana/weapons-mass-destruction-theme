20d3caa629ed6e2f348a4b2d03a3ed84c18d834e
yoemrattana <yoemrattana168@gmail.com>
2019-01-29 13:53:41 +0700

    lang english to slider

--- template-parts/content-slideshow-page-home.php
+++ template-parts/content-slideshow-page-home.php
@@ -2,8 +2,12 @@
 	<div class="container" style="max-width: 100%; padding: 0">
 		<div class="row">
 			<div class="col-sm-12">
-				<?php //echo do_shortcode('[metaslider id="254"]'); ?>
-				<?php echo do_shortcode('[metaslider id="410"]'); ?>
+				<?php if (get_bloginfo("language") == 'km') : ?>
+					<?php //echo do_shortcode('[metaslider id="254"]'); ?>
+					<?php echo do_shortcode('[metaslider id="410"]'); ?>
+				<?php else : ?>
+					<?php echo do_shortcode('[metaslider id="627"]'); ?>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
